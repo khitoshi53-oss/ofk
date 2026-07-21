853bb42d0eb08d65d380f0d3c12702fea1dc06a1
khitoshi53-oss <k.hitoshi53@gmail.com>
2026-07-21 16:03:33 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,9 @@
-// ▼▼▼ ここをあなたのFirebaseプロジェクトの値に書き換えてください ▼▼▼
-// 取得方法は SETUP_GUIDE.md を参照してください。
 window.FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDDZLs48hms1943E1VqtHN87Swz6W-yNYo",
+  authDomain: "officek-4bd3f.firebaseapp.com",
+  projectId: "officek-4bd3f",
+  storageBucket: "officek-4bd3f.firebasestorage.app",
+  messagingSenderId: "96009912431",
+  appId: "1:96009912431:web:0cf9f5185814b83980cd9a",
+  measurementId: "G-BZGNL51LQY"
 };
-// ▲▲▲ ここまで ▲▲▲
